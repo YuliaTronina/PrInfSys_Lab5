812f614cad59ecb2cb577f91eaa04644954f5ce1
Artur_Mukhametdinov <ekb-led2012@yandex.ru>
2024-10-04 07:58:46 +0500

    Added description of additional classes

--- src/additionalFunctions/DocumentReader.java
+++ src/additionalFunctions/DocumentReader.java
@@ -1,5 +1,25 @@
 package additionalFunctions;
 
-public class DocumentReader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class DocumentReader {
+    public static ArrayList<String> fileLineReader(String filename) { // на вход подаем строку с путем к файлу
+        BufferedReader reader; // создаем ридер для чтения из файла
+        ArrayList<String> readLines = new ArrayList<>(); // создаем массив строк
+        try {
+            reader = new BufferedReader(new FileReader(filename)); // указываем из какого файла читать
+            String line = reader.readLine(); // читаем строку
+            while (line != null) { // если не конец файла
+                readLines.add(line); //записываем прочитанное в массив строк
+                line = reader.readLine(); // переходим на следующую строку
+            }
+            reader.close(); // закрываем ридер
+        } catch (IOException e) {
+            throw new RuntimeException(e); // выбрасываем ошибку в случае чего
+        }
+        return readLines; // возвращаем массив строк
+    }
 }
--- src/additionalFunctions/SplitLines.java
+++ src/additionalFunctions/SplitLines.java
@@ -1,4 +1,20 @@
 package additionalFunctions;
 
+import java.util.ArrayList;
+
 public class SplitLines {
+    public static String subStrings (ArrayList<String> lines, int index) { // передаем в метод массив строк и индекс элемента, который будем редактировать
+        String str = lines.get(index); // записываем третий элемент массива в строку
+        str = str.substring(0, str.length() - 1); // создаем подстроку, отрезаем последний символ
+        str = str.substring(1); // создаем подстроку, отрезаем первый символ
+        return str; // возвращаем итоговую строку без первого и последнего символа
+    }
+    public static double [] splitLines (String lines) { // передаем строку, полученную из предыдущего метода
+        String [] words = lines.split(";"); // создаем массив строк и помещаем в него строки с разделителем
+        double [] result = new double[words.length]; // создаем массив вещественных чисел
+        for (int i = 0; i < words.length; i++) {
+            result[i] = Double.parseDouble(words[i]); // помещаем в массив предварительно приведенные к вещественным числам строки
+        }
+        return result; // возвращаем этот массив
+    }
 }
