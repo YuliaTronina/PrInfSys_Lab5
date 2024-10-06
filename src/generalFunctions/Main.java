1cb92730303f8fb80c47c917d2b59bcdbea1c346
Artur_Mukhametdinov <ekb-led2012@yandex.ru>
2024-10-06 18:30:39 +0500

    The file path has been added

--- src/generalFunctions/Main.java
+++ src/generalFunctions/Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) { //Главный метод, точка входа в программу
-        ArrayList<String> list  = DocumentReader.fileLineReader("src/generalFunctions/file1"); // читаем данные из файла и сохраняем в массив
+        ArrayList<String> list  = DocumentReader.fileLineReader("src/file1"); // читаем данные из файла и сохраняем в массив
         double [] averageRecoveryTime = SplitLines.splitLines(SplitLines.subStrings(list,2)); // массив с пороговыми значениями для параметра Тв, взятый из прочитанного файла
         double [] estDurTransIO = SplitLines.splitLines(SplitLines.subStrings(list,4)); // массив с пороговыми значениями для параметра Тп, взятый из прочитанного файла
         double probability = ProbabilityWithoutFailures.probabilityCalculations(list); // рассчет P
